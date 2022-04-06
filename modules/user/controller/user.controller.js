5f2f85065e1fb2d9505a3ec6830c809767f0628a
Ahmadsaidnouh <es-ahmad.nouh2025@alexu.edu.eg>
2022-04-06 17:00:54 +0200

    edit getUser

--- modules/user/controller/user.controller.js
+++ modules/user/controller/user.controller.js
@@ -215,56 +215,59 @@ const userModel = require("../../../DB/models/user.model");
 //     }
 // }
 const updateBalance = async (req, res) => {
-    let {userId, newBalance} = req.body;
+    let { userId, newBalance } = req.body;
     try {
-        const user = await userModel.findOne({_id: userId});
-        if(user) {
-            const updatedUser = await userModel.findByIdAndUpdate(useId, {currentBalance: newBalance}, {new: true})
-            res.json({message: "Done", updatedUser});
+        const user = await userModel.findOne({ _id: userId });
+        if (user) {
+            const updatedUser = await userModel.findByIdAndUpdate(useId, { currentBalance: newBalance }, { new: true })
+            res.json({ message: "Done", updatedUser });
         }
         else {
-            res.status(400).json({message: "Error"});
+            res.status(400).json({ message: "Error" });
         }
     } catch (error) {
-        res.status(400).json({message: "Error"});
+        res.status(400).json({ message: "Error" });
     }
 }
 
 const getUser = async (req, res) => {
-    let {userId} = req.params;
+    let { userId } = req.params;
     try {
-        const user = await userModel.findOne({_id: userId});
-        const users = await userModel.find({});
-        if(user) {
-            res.json({message: "Done", user, users});
+        const user = await userModel.findOne({ _id: userId });
+        const temp = await userModel.find({});
+        var users = temp.filter(function (value) {
+            return value._id != userId;
+        });
+        if (user) {
+            res.json({ message: "Done", user, users });
         }
         else {
-            res.status(400).json({message: "Error"});
+            res.status(400).json({ message: "Error" });
         }
     } catch (error) {
-        res.status(400).json({message: "Error"});
+        res.status(400).json({ message: "Error" });
     }
 }
 const getAllUsers = async (req, res) => {
     const users = await userModel.find({});
-    if(users.length) {
-        res.json({message: "Done", users});
+    if (users.length) {
+        res.json({ message: "Done", users });
     }
     else {
-        res.status(400).json({message: "Error"});
+        res.status(400).json({ message: "Error" });
     }
 }
-const addUser = async(req, res) => {
+const addUser = async (req, res) => {
     try {
-        
-        
-        let {name, email, currentBalance} = req.body;
+
+
+        let { name, email, currentBalance } = req.body;
         // const addedUser = await userModel.insertMany({name, email, currentBalance})
-        const createdUser = new userModel({name, email, currentBalance});
+        const createdUser = new userModel({ name, email, currentBalance });
         const addedUser = await createdUser.save();
-        res.json({message:"done", addedUser});
+        res.json({ message: "done", addedUser });
     } catch (error) {
-        res.status(400).json({message:"Error"})
+        res.status(400).json({ message: "Error" })
     }
 }
 
